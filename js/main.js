d789d95442605b7c55cd44db17d90d774769bc5a
VITT.Ivanov <vitaly.ivanov@mail.ru>
2026-03-13 13:41:06 +0300

    разметка для мобильной версии

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+// появление списка advantages 
+const block = document.querySelector('.advantages');
+const button = document.querySelector('.advantages__title');
 
+button.addEventListener('click', () => {
+  block.classList.toggle('active');
+});
